6c6a4c0a6560ad318ab29c1a9cd84109d93a741d
Javier Fernandez <javierfernandez.2smx@gmail.com>
2024-01-26 10:31:42 +0100

    Edited message App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        In the process of creation, <code>this is a preview</code> ✌️.
+        In creation process, <code>this is a preview</code> ✌️.
         </p>
         <a
           className="App-link"
